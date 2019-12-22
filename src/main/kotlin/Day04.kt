319300751eb037f562f689434f00c9989cf2f948
Tom Stuttard <tom.stuttard@stream-technologies.com>
2019-12-22 23:51:26 +0000

    Day4 both stars

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,58 @@
+class Day04 {
+    fun isValidPassword(possiblePassword: String): Boolean {
+
+
+        if (possiblePassword.length != 6) {
+            return false
+        }
+
+        var isSameOrHasIncreased = true
+
+        val groupedCharacters = mutableListOf<Pair<Char, Int>>()
+        var currentListPointer = 0
+
+        for ((index, character) in possiblePassword.withIndex()) {
+
+            if (index == 0) {
+                groupedCharacters.add(Pair(character, 1))
+                continue
+            }
+
+            if (character == groupedCharacters[currentListPointer].first) {
+                val characterCount = groupedCharacters[currentListPointer].second + 1
+                groupedCharacters[currentListPointer] = Pair(character, characterCount)
+            } else {
+                groupedCharacters.add(Pair(character, 1))
+                currentListPointer++
+            }
+
+
+            if (character.toInt() < possiblePassword[index-1].toInt()) {
+                isSameOrHasIncreased = false
+            }
+        }
+
+        if (groupedCharacters.count { it.second == 2 } == 0) {
+            return false
+        }
+
+        if (!isSameOrHasIncreased) {
+            return false
+        }
+
+
+
+        return true
+    }
+
+    fun findValidPasswords(passwordRange: String): List<String> {
+        val passwordStart = passwordRange.split('-').first()
+        val passwordEnd = passwordRange.split('-').last()
+
+
+        val passwords = (passwordStart.toInt()..passwordEnd.toInt()).map { it.toString() }
+
+        return passwords.filter { isValidPassword(it) }
+    }
+
+}
---
+++ src/test/kotlin/Day04Test.kt
@@ -0,0 +1,48 @@
+import org.spekframework.spek2.Spek
+import java.math.BigDecimal
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class Day04Test : Spek({
+
+    val day04 by memoized { Day04() }
+
+    test("not enough digits") {
+        assertFalse { day04.isValidPassword("11111") }
+    }
+
+    test("not enough decreasing pair of digits") {
+        assertFalse { day04.isValidPassword("223450") }
+    }
+
+    test("no double digits") {
+        assertFalse { day04.isValidPassword("123789") }
+    }
+
+    test("valid password digits never decrease") {
+        assertTrue { day04.isValidPassword("112233") }
+    }
+
+    test("valid password 223333") {
+        assertTrue { day04.isValidPassword("223333") }
+    }
+
+    test("invalid password 111111") {
+        assertFalse { day04.isValidPassword("111111") }
+    }
+
+    test("valid password 111122") {
+        assertTrue { day04.isValidPassword("111122") }
+    }
+
+    test("invalid repeated number part of larger group") {
+        assertFalse { day04.isValidPassword("123444") }
+    }
+
+
+    test("fill me in too") {
+        val validPasswords = day04.findValidPasswords("158126-624574")
+        assertEquals(0, validPasswords.count())
+    }
+})

9954e8ea41b01418414a56ac73d7835e53fa1491
Tom Stuttard <tom.stuttard@stream-technologies.com>
2019-12-23 17:54:26 +0000

    Use correct order of expected and actual in assert equals

--- src/test/kotlin/Day02Spec.kt
+++ src/test/kotlin/Day02Spec.kt
@@ -4,30 +4,30 @@ import kotlin.test.assertEquals
 
 class Day02Spec : Spek({
     test("single chunk add operation") {
-        assertEquals(Day02("1,0,0,0,99").executeProgram(), "2,0,0,0,99")
+        assertEquals("2,0,0,0,99", Day02("1,0,0,0,99").executeProgram())
     }
 
     test("single chunk multiply operation") {
-        assertEquals(Day02("2,3,0,3,99").executeProgram(), "2,3,0,6,99")
+        assertEquals("2,3,0,6,99", Day02("2,3,0,3,99").executeProgram())
     }
 
     test("multiple chunk multiply operation with storage outside first chunk") {
-        assertEquals(Day02("2,4,4,5,99,0").executeProgram(), "2,4,4,5,99,9801")
+        assertEquals("2,4,4,5,99,9801", Day02("2,4,4,5,99,0").executeProgram())
     }
 
     test("multiple chunk add operation where initial halt opcode changes") {
-        assertEquals(Day02("1,1,1,4,99,5,6,0,99").executeProgram(), "30,1,1,4,2,5,6,0,99")
+        assertEquals("30,1,1,4,2,5,6,0,99", Day02("1,1,1,4,99,5,6,0,99").executeProgram())
     }
 
     test("file chunk") {
         val output = Day02(File("src/test/resources/day02.txt")).executeProgram(ProgramInput(12, 2))
-        assertEquals(output.split(",")[0], "3101878")
+        assertEquals("3101878", output.split(",")[0])
     }
 
     test("find inputs that produce output") {
         val expectedProgramInput = ProgramInput(84, 44)
-        assertEquals(Day02(File("src/test/resources/day02.txt")).findInputsFromOutput(19690720), expectedProgramInput)
-        assertEquals(expectedProgramInput.noun * 100 + expectedProgramInput.verb, 8444)
+        assertEquals(expectedProgramInput, Day02(File("src/test/resources/day02.txt")).findInputsFromOutput(19690720))
+        assertEquals(8444, expectedProgramInput.noun * 100 + expectedProgramInput.verb)
 
     }
 })
